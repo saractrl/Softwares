d1bc401e6a57d89c626a9cda2ebf2a859ecbbfa5
Sara Ramirez <sara.ctrl@outlook.com>
2023-06-25 23:06:55 +0200

    FINISHED

--- db.py
+++ db.py
@@ -1,95 +1,58 @@
-# TO DO: Imports
 import os
 import pymongo
 
 class Db():
-
+    
     def __init__(self):
         self.connectionstring = os.getenv("CS_MONGODB")
         self.client = pymongo.MongoClient(self.connectionstring)
+        self.db = self.client["Softwares"]
+        self.col = self.db["Keys"]
         self.db_list = self.client.list_database_names()
-        self.col_list = self.client.list_collection_names()
-        self.db = "Softwares"
-        self.col = "Keys"
 
-    def create_db(self):
-        self.client[self.db]
-        print("Database created!")
-
-    def create_col(self):
-        self.client[self.db][self.col]
-        print("Collection created!")
-
-    def check_db(self):
+    def create_db_col(self):
         if self.db not in self.db_list:
-            db_input = input("Type 'y' if you want to create a database and 'n' if not.")
+            db_input = input("No database and collection found. Press 'y' if you want to create one and 'n' if not.")
+        else:
             if db_input.lower() == "y":
-                self.create_db()
+                self.db = self.client["Softwares"]
+                self.col = self.db["Keys"]
+                print("Database and collection created.")
             else:
-                print(exit)
-                exit()
-
-    def check_col(self):
-        if self.col not in self.col_list:
-            col_input = input("Type 'y' if you want to create a collection and 'n' if not.")
-            if col_input.lower() == 'y':
-                self.create.col()
-            else:
-                print(exit)
+                print("Program ended.")
                 exit()
 
 
     def show_sw(self):
-        softwares = self.client[self.db][self.col].find()
-        for index, software in enumerate(softwares, start = 1):
-            print(index, ':', software)
+        print("All the softwares in the database:")
+        softwares = list(self.col.find())
+        for index, software in enumerate(softwares, start=1):
+            print(f"{index} :  {software['name']}")
 
-    def insert_sw():
-        software_input = "Type name of software."
+    def insert_sw(self):
+        software_input = input("Type name of software.")
+        maker_input = input("Type maker of the software.")
+        software = {"name" : software_input, "maker" : maker_input, "keys" : []}
+        self.col.insert_one(software)
+        print("Software added!:)")
+
+
+    def show_keys(self):
+        number_input = int(input("Type the number of software.")) -1
+        software = self.col.find()[number_input]
+        keys = software.get("keys", [])
+        print("Keys:")
+        for key in keys:
+            print(key)
         
-        maker_input = "Type maker of the software."
 
-        Db.show_sw()
-
-
-    def show_keys():
-        number_input = "Type the number of software."
-
-
-    def insert_key():
-        number_input = "Type the number of software."
-        insert_key = "Insert a key."
-
-    
-    # if db not in db_list:
-    #     db_input = input("Type 'yes' if you want to create a database and 'no' if not.")
-    #     if db_input == "yes":
-    #         #TO DO: CReate Database
-    #         db = client[db]
-    #         print("Database created!")
-    #         #break --> loop?
-    #     elif db_input == "no":
-    #         #TO DO: QUit program
-    #         print(exit)
-    #         exit()
-
-    # if col not in col_list:
-    #     col_input = input("Type 'yes if you want to create a collection and 'no' if not.")
-    #     if col_input == "yes":
-    #         #To do: Create collection in SOftware database
-    #     else:
-    #         #To do: QUIt program
-    #         print(exit)
-    #         exit()
+    def insert_key(self):
+        number_input = int(input("Type the number of software.")) -1
+        insert_key = input("Insert a key:")
+        software = self.col.find()[number_input]
+        software_name = software["name"]
+        self.col.update_one({"name": software_name}, {"$set": {"keys" : insert_key}})
+        print("Added key.")
 
 
     
-    # #TO DO: Softwares mit Laufnummer anzeigen
-    # for db in db_list:
-    #     print(db)
-    # insert_input = input("Type 'i' if you want to insert a new software, 'k' if you want to show all the key, 'ik' if you want to insert a new key and 'q' if you want to quit the program.")
- 
-
-
-        
-
--- main.py
+++ main.py
@@ -1,34 +1,39 @@
 from db import Db
 
-def __main__():
 
+def main():
     database = Db()
-    database.check_db()
-    database.check_col()
-
-    database.show_sw()
 
     while True:
-        print("Choose an option:")
-        print("Type 'i' to insert new software.")
-        print("Type 'k' to show keys of a software.")
-        print("Type 'ik' to insert a new key into a software.")
-        print('Type q to quit program.')
+        database.show_sw()
+        print("\nChoose one of these options please: ")
+        print("Type 'i' to insert a new software: ")
+        print("Type 'k' if you want to see the keys of a software: ")
+        print("Type 'ik' if you want to insert a new key belonging to a software: ")
+        print("Type 'q' if you want to quit the program: ")
 
-        insert_input = input("Your choice:")
+        insert_input = input("")
 
         if insert_input.lower() == "i":
             database.insert_sw()
+            database.show_sw()
         elif insert_input.lower() == 'k':
             database.show_keys()
+            database.show_sw()
         elif insert_input.lower() == 'ik':
             database.insert_key()
+            database.show_sw()
         elif insert_input.lower() == 'q':
-            print(quit)
+            print("\n Leaving program... :( ")
             quit()
+        else:
+            print("Try again.")
+            False
+           
+
 
 if __name__ == "__main__":
-    __main__()
+    main()
 
 
 
